246f219b23da1525d520dbabbc8cd7dee0fcaee5
GitRepo-techh <farhanrehmat2007@gmail.com>
2026-05-18 04:41:53 -0700

    Add grayscale method to filters class
    Implement grayscale conversion for images in filter.py.

--- filter.py
+++ filter.py
@@ -1,7 +1,16 @@
 class filters:
 
     def graysclae(self, img):
-        pass
+        image_load = image.load()
+      width, height = image.size
+      for row in range(width):
+         for column in range(height):
+            r,g,b = image_load[row,column]
+            grey = round(0.299*r + 0.587*g + 0.114*b)
+            image_load[row,column]= [grey,grey,grey]
+      save = image.save("file_name")
+      return save
+
 
     def blur(self, img):
         pass
