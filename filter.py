c2ec33a0b46933781b569150ca8544dd6319c18f
FuzzyTrout <zohaib233146.com>
2026-05-18 17:40:37 +0500

    pleej

--- filter.py
+++ filter.py
@@ -1,15 +1,15 @@
 class filters:
 
-    def grayscale(self, image):
+    def graysclae(self, img):
         image_load = image.load()
         width, height = image.size
         for row in range(width):
-            for column in range(height):
-                r, g, b = image_load[row, column]
-                grey = round(0.299 * r + 0.587 * g + 0.114 * b)
-                image_load[row, column] = (grey, grey,  grey)
-        
-        return image
+           for column in range(height):
+             r,g,b = image_load[row,column]
+             grey = round(0.299*r + 0.587*g + 0.114*b)
+             image_load[row,column]= [grey,grey,grey]
+        save = image.save("file_name")
+        return save
 
-    def blur(self, image):
+    def blur(self, img):
         pass
